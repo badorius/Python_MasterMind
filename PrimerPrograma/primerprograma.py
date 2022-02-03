44f4ef50146978ab144bc157f76f1f48f7ec9824
darthv <salvadormuntane@gmail.com>
2022-02-03 20:49:42 +0100

    Add primerprograma and notes

---
+++ PrimerPrograma/primerprograma.py
@@ -0,0 +1,14 @@
+#Introducimos tres números con la funcion input y la asignamos a la variable convertiendo esta de tipo int
+numero1=int(input("Introduce el primero número:"))
+numero2=int(input("Introduce el segundo número:"))
+numero3=int(input("Introduce el tercer  número:"))
+
+#Con la función max y min búscamos el número mayor y menor
+
+print ("El número mayor es:" + str(max(numero1, numero2, numero3)))
+print ("El número menor es:" + str(min(numero1, numero2, numero3)))
+
+"""
+Con triple doble comillas creamos comentario en bloque, con # creamos comentario de línea
+"""
+
