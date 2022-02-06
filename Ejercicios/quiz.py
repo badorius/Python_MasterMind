4b960a7766f13648326cd8e9897d1439ca3951f3
darthv <salvadormuntane@gmail.com>
2022-02-06 20:43:18 +0100

    Add Tabla de quesos 1

---
+++ Ejercicios/quiz.py
@@ -0,0 +1,11 @@
+
+titulo = "Biernvenido al Test sobre el queso"
+print("\n" + titulo + "\n" + "-" * len(titulo) + "\n")
+
+puntuacion = 0
+
+opcion = input("""Pregunta 1: que haces cuando ves una tabla de quesos?
+A - Salgo corriendo.
+B - Pruebo una de los quesos o incluso varios.
+C - No puedo exitar devorarla
+""")
