cab0bf19d844fce19d430aedc9eeba1279004abc
badorius <salvadormuntane@gmail.com>
2022-02-22 23:47:58 +0100

    Add reto web makdown

--- reto_python_atareao/retos/drupal_to_markdown.py
+++ reto_python_atareao/retos/drupal_to_markdown.py
@@ -8,29 +8,21 @@ path = os.getcwd()
 output_directory = path + "/output"
 node = 100
 
-try:
-    os.mkdir(output_directory)
-except OSError:
-    print ("Creation of the directory %s failed" % output_directory)
-else:
-    print ("Successfully created the directory %s " % output_directory)
+for node in range(101):
+    url = "http://www.badorius.com/?q=node/" + str(node)
+    file_md = str(output_directory) + str(node) + ".md"
 
+    html = requests.get(url).text
 
+    # convert html to markdown
+    h = markdownify.markdownify(html, heading_style="ATX")
 
-url = "http://www.badorius.com/?q=node/" + str(node)
-file_md = str(output_directory) + str(node) + ".md"
+    # print markdown
+    #print(h)
 
-html = requests.get(url).text
-
-# convert html to markdown
-h = markdownify.markdownify(html, heading_style="ATX")
-
-# print markdown
-#print(h)
-
-carname="Swift"
-caryear="2000"
-carcolor="white"
-file = open(file_md, "w")
-file.write(h)
-file.close
+    carname="Swift"
+    caryear="2000"
+    carcolor="white"
+    file = open(file_md, "w")
+    file.write(h)
+    file.close
