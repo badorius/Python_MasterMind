5bc3e6e90c36234a6c96936512eaf3cda82579a5
badorius <salvadormuntane@gmail.com>
2022-06-09 21:23:14 +0200

    Add twit youtube face

---
+++ Ejercicios/modulo_funciones/hackerscript_history_twitter_youtube_facebook.py
@@ -0,0 +1,73 @@
+import os
+import re
+import time
+from time import sleep
+from random import randrange
+from pathlib import Path
+import sqlite3
+
+#home_path = "/home/" + os.getlogin()
+home_path = "{}".format(Path.home())
+desktop_path = home_path + "/Desktop/"
+timestr = time.strftime("%Y%m%d-%H%M%S")
+FILE = desktop_path + timestr + "eps3.7dont-delete-me.ko"
+history_path = "/home/" + os.getlogin() + "/.config/google-chrome/Default/History"
+
+def delay_action():
+    n_hours = randrange(1, 4)
+    print("Durmiendo {} Segundos...".format(n_hours))
+    sleep(n_hours)
+
+
+def remove_hacker_file():
+    os.remove(FILE)
+
+
+def create_hacker_file():
+    if os.path.exists(FILE):
+        append_write = 'a'  # append if already exists
+    else:
+        append_write = 'w'  # make a new file if not
+
+    hacker_file = open(FILE, append_write)
+    hacker_file.write("You have been pwned\n")
+    return hacker_file
+
+
+def get_chrome_history():
+    urls = None
+    while not urls:
+        try:
+            connection = sqlite3.connect(history_path)
+            cursor = connection.cursor()
+            cursor.execute("SELECT title, last_visit_time, url FROM urls ORDER BY last_visit_time DESC")
+            urls = cursor.fetchall()
+            connection.close()
+            return urls
+        except sqlite3.OperationalError:
+            sleep(3)
+
+
+def check_twitter_profiles_and_scare_user(hacker_file, chrome_history):
+    maxhist = 1
+    profiles_visited = []
+    url_exceptions = ["home", "notifications", "explore", "login"]
+
+    for item in chrome_history:
+        results = re.findall("https://twitter.com/([A-Za-z0-9]+)$", item[2])
+        if results and results[0] not in url_exceptions:
+            profiles_visited.append(results[0])
+    hacker_file.write("He visto que has estado husmeando en los perfiles de {}...".format(", ".join(profiles_visited)))
+
+def main():
+    print(FILE)
+
+    delay_action()
+    hacker_file = create_hacker_file()
+    chrome_history = get_chrome_history()
+    check_twitter_profiles_and_scare_user(hacker_file, chrome_history)
+
+
+
+if __name__ == "__main__":
+    main()
