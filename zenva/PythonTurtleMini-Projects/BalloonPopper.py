85c1c216e4c207448eb0752bdfb262a3b73ff9bf
badorius <salvadormuntane@gmail.com>
2023-03-25 21:28:45 +0100

    Start zeneva

--- zenva/PythonTurtleMini-Projects/BalloonPopper.py
+++ zenva/PythonTurtleMini-Projects/BalloonPopper.py
@@ -1,13 +1,34 @@
 from turtle import *
+from random import randint
 
 diameter = 40
 pop_diameter = 100
+colors = ["red", "blue", "yellow", "green", "red", "black"]
+bgcolor("black")
+
+
+def change_color():
+    color(colors[randint(0, 6)])
+
 
 def draw_ballon():
-    color("red")
     dot(diameter)
 
 
-draw_ballon()
+def inflate_ballon ():
+    global diameter
+    diameter = diameter + 10
+    draw_ballon()
 
+    if diameter >= pop_diameter:
+        clear()
+        diameter = 40
+        write("POP!")
+        change_color()
+
+
+change_color()
+draw_ballon()
+onkey(inflate_ballon, "Up")
+listen()
 done()
