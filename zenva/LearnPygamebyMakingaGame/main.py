800d626addba40dce6913f1e23cb1319a8972fbc
badorius <salvadormuntane@gmail.com>
2023-04-04 21:10:22 +0200

    Add pygame

--- zenva/LearnPygamebyMakingaGame/main.py
+++ zenva/LearnPygamebyMakingaGame/main.py
@@ -6,27 +6,31 @@ width = 800
 height = 800
 white_colour = (255, 255, 255)
 
-# Creates the game window
 game_window = pygame.display.set_mode((width, height))
 
-# Creating a clock variable
 clock = pygame.time.Clock()
 
-# Game loop
-while True:
-    # What's inside this loop will be repeated continuosly
-    
-    # Handle events
-    # Execute logic
-    # Update display
-    
-    # Colors the background white
-    game_window.fill(white_colour)
-    # Updates the changes to the window 
-    pygame.display.update()
-    
-    # Setting how many times per second we want to update the game window
-    clock.tick(60)
+# Our game loop is now within a function
+def run_game_loop():
+    while True:
+        # Handle events
+        events = pygame.event.get()
+        # Loops for all occurring events
+        for event in events:
+            # Checks if any of them is the QUIT event
+            if event.type == pygame.QUIT:
+                # Breaks the game loop by exiting the whole function
+                return
+              
+        # Execute logic
+        # Update display
+        game_window.fill(white_colour)
+        pygame.display.update()
+        
+        clock.tick(60)
+        
+# Calls the run_game_loop function       
+run_game_loop()
   
 pygame.quit()
 quit()
